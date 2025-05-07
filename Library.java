5e848fceb06f06692c4bf5e5edafe6f08499a50d
ReedandWrite <85583895+ReedandWrite@users.noreply.github.com>
2025-05-07 16:17:28 -0400

    Updated Library.java

--- Library.java
+++ Library.java
@@ -1,39 +1,87 @@
 import java.util.ArrayList;
 public class Library {
-    private ArrayList<Books> books;
-    private ArrayList<User> users;
+    private ArrayList<Book> books;
+    private ArrayList<Person> people;
     
-    // Constructor
     public Library() {
         books = new ArrayList<>();
-        users = new ArrayList<>();
+        people = new ArrayList<>();
     }
     
-    // Add user to library
-    public void addUser(User user) {
+    public void addPerson(Person person) {
+        people.add(person);
     }
     
-    // Delete user from library
-    public boolean deleteUser(String userID) {
-        return true;
+    public boolean deletePerson(String personName) {
+        for (int i = 0; i < people.size(); i++) {
+            if (people.get(i).getName().equals(personName)) {
+                people.remove(i);
+                return true;
+            }
+        }
+        return false;
     }
     
-    // Print directory of all users
     public void printDirectory() {
+        System.out.println("Library Users:");
+        for (Person person : people) {
+            System.out.println("- " + person.getName());
+        }
     }
     
-    // Checkout book to user
-    public boolean checkoutBook(String userID, int bookID) {
-        return true;
+    public void addBook(Book book) {
+        books.add(book);
     }
     
-    // Checkin book from user
-    public boolean checkinBook(String userID, int bookID) {
-        return true;
+    public boolean checkoutBook(String personName, int bookID) {
+        Person person = findPerson(personName);
+        Book book = findBook(bookID);
+        
+        if (person != null && book != null) {
+            return person.checkOutBook(book);
+        }
+        return false;
     }
     
-    // Search books by name
-    public ArrayList<Books> bookSearch(String searchTerm) {
-        return new ArrayList<>();
+    public boolean checkinBook(String personName, int bookID) {
+        Person person = findPerson(personName);
+        Book book = findBook(bookID);
+        
+        if (person != null && book != null) {
+            return person.returnBook(book);
+        }
+        return false;
+    }
+    
+    public ArrayList<Book> bookSearch(String searchTerm) {
+        ArrayList<Book> results = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getBookName() != null && book.getBookName().contains(searchTerm)) {
+                results.add(book);
+            }
+        }
+        return results;
+    }
+    
+    private Person findPerson(String personName) {
+        for (Person person : people) {
+            if (person.getName().equals(personName)) {
+                return person;
+            }
+        }
+        return null;
+    }
+    
+    private Book findBook(int bookID) {
+        for (Book book : books) {
+            if (book.getBookID() == bookID) {
+                return book;
+            }
+        }
+        return null;
+    }
+    
+    public ArrayList<Book> getAllBooks() {
+        return books;
     }
 }
