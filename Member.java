b7c1ad30f5fb37b7abeda8f5244318a8fb3159fd
GinaSaphira <gmvaldez@iu.edu>
2025-05-08 20:10:44 -0500

    Member.java
    Phase 3 - Final Submission for Member.java class.

--- Member.java
+++ Member.java
@@ -1,6 +1,6 @@
 /*
  INFO-C211 | Group Project 
- Virtual Library: Phase 2
+ Virtual Library: Phase 3
  Gina Valdez
  Member Class Stub | Represents a user who checks out books from the library.
 */
@@ -19,50 +19,61 @@ public class Person {
     private ArrayList<Book> checkedOutBooks;
 
 
-    // Constructor; Add person's name
-    public Person(String name) {
-    }
-
     // Constructor; sets name and a custom max book limit
-    public Person(String name, int maxBooks) {
+    public Person(String name) {
+        this.name = name;
+        this.maxBooks = 5;
+        this.checkedOutBooks = new ArrayList<>();
     }
 
     // Get the person's name
     public String getName() {
-        return null;
+        return name;
     }
 
 
     // Update the person's name
     public void setName(String name) {
+        this.name = name;
     }
 
 
     // See the list of checked out books
     public ArrayList<Book> getCheckedOutBooks() {
-        return null;
+        return checkedOutBooks;
     }
 
 
     // Getter method for number of books allowed to borrow in total
     public int getMaxBooks() {
-        return 0;
+        return maxBooks;
     }
 
 
     // Setter method for the max book limit, which will be 5
     public void setMaxBooks(int maxBooks) {
+        this.maxBooks = maxBooks;
     }
 
 
     // Function to check out a book
     public boolean checkOutBook(Book book) {
+        if (checkedOutBooks.size() < maxBooks && !book.getBookStatus()) {
+            checkedOutBooks.add(book);
+            book.borrowed();
+            return true;
+        }
         return false;
     }
 
     
     // Function to return book
     public boolean returnBook(Book book) {
+        if (checkedOutBooks.contains(book)) {
+            checkedOutBooks.remove(book);
+            book.returned();
+            return true;
+        }
         return false;
     }
 }
